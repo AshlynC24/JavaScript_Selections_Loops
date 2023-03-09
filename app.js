5ae0810c2a4381643fd1249aa87ad964e5ea78c4
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-03-09 14:40:06 -0500

    Complete

--- app.js
+++ app.js
@@ -2,6 +2,116 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+//create a for loop that evaluates numbers from 1-100 
+//inside the body of the loop write if/else section 
+for (let i = 0; i < 100, i ++){
+    if(i % 2 == 0 ){
+        continue;
+    }
+    else{
+        console.log(i)
+    }
+}
+//Exercise 3
+let i = i; 
+while(i <=100){
+    if(i % 2 != 0){
+    console.log(i);
+
+    }
+    i++;
+}
+let t = 1;
+do{
+    if(t % 2 != o){
+        console.log(t);
+    }
+    t++;
+}while(t < 100);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let i =0; i <100, i++)
+{
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+    else{ 
+        if (i % 3 ==0 && i % 5 ==0) {
+        console.log("FizzBuzz");}
+    }
+}
+
+//Exercise 3 
+let g = 1;
+while(g <= 100){
+    let output = "";
+
+    if(g % 3 == 0){
+        output = "Fizz";
+    }
+
+    if(g % 5 == 0){
+        output = "Buzz";
+    }
+    
+    if(g % 3 ==0 && g % 5 ==0){
+        output = "FizzBuzz";
+    }
+
+    console.log({g}, {output});
+}
+
+let x = 1;
+do{
+    let output = "";
+    if(x % 3 ==0){
+        output += "Fizz";
+    }
+
+    if(x % 5 == 0){
+        output += "Buzz";
+    }
+
+    if(x% 3 ==0 && x % 5 ==0 ){
+        output += "FizzBuzz";
+    }
+
+} 
+while(x <= 100);
+
+//Exercise 4 
+let value = Math.round((Math.random() * 500)); //creates a random number between 0 and 500 
+let n = Math.round(Math.random()* (500-100) + 100) // creates a random number between 100 and 500 
+
+for (let i =1; i <= n; i++){
+    if(i == numberToFind){
+        console.log('Found', {numberToFind}, '!');
+        break;
+    }
+    if(i == n){
+        console.log('Did not find', {numberToFind}, 'within 1 -', {n});
+
+    }
+}
+
+//Exercise 5 
+let fizzDivisor = Math.round(Math.random() * (10-1) + 1);
+let buzzDivisor = Math.round(Math.random() * (10-1)+ 1);
+let start = Math.round(Math.random()* (10-1)-1);
+let end = Math.round(Math.random()* (1000 -100) + 100);
+
+for(let i = start; i <= end; i ++){
+    let output= "";
+    if(i % fizzDivisor == 0){
+        output = "Fizz";
+    }
+    if(i % buzzDivisor == 0){
+        output = "Buzz";
+    }
+    console.log({i}, {output});
+}
+console.log(fizzDivisor, buzzDivisor);
